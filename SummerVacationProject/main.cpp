603f10fac79c8e6bbd8d7da17911eed408e7bac2
phj1027 <qkrgywjd981027@naver.com>
2021-06-27 03:05:40 +0900

    2st SFML
    sprite와 texture에 대해 알아봄
    이미지 하나 가져와서 출력해봄

--- SummerVacationProject/main.cpp
+++ SummerVacationProject/main.cpp
@@ -7,25 +7,28 @@ using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(500, 500), "Window"); // 500x500짜리 window창
+	RenderWindow window(VideoMode(500, 500), "Window"); 
 
-	// 사각형 오브젝트 rs
-	RectangleShape rs;
+	// Sprite스프라이트 : 사진을 덮어쓸수있는 오브젝트 (절대 사이즈가 없음)
+	// ㄴ> scaling해줘야함 - texture사이즈를 그대로 가져오기때문에 이를 배수를 곱해서 사이즈 변경 가능 
+	Sprite sp; 
+	Texture tx; // texture를 입히면 sprite가 됨 
 
-	// vector2f = float자료형 두개짜리(x,y) (*2.0은 double자료형 2.f는 float자료형)
-	rs.setSize(Vector2f(100.f, 100.f)); //사이즈
-	rs.setFillColor(Color::Green); // 색상
-	rs.setPosition(Vector2f(250.f, 250.f)); // 위치 (*오브젝트의 피봇이 좌상단 기준이기때문에 윈도우 한가운데에 위치하지않음)
-	rs.setOrigin(rs.getSize() / 2.f); // 오브젝트의 피봇을 좌상단에서 오브젝트의 한가운데로 옮겨줌
+	tx.loadFromFile("Textures/1.jpg"); // main.cpp 위치 기준 상대경로
 
-	while (window.isOpen())//윈도우가 오픈되어잇을때
+	sp.setScale(0.5f, 0.5f); // 사이즈 조절 - 소수일수록 작아지고 1이상은 커지는 것
+	sp.setTexture(tx); // sp에 texture입힘 
+
+	// 피봇 스프라이트 중심으로 바꿈 
+	sp.setOrigin(tx.getSize().x / 2.f, tx.getSize().y / 2.f); // sp는 절대사이즈없기때문에 tx사이즈 이용해서 바꿈
+	sp.setPosition(250.f, 250.f); // 위치 설정
+
+	while (window.isOpen())
 	{
-		window.clear(); // 오브젝트를 움직일때 그 전에것들을 한번씩 지워줌
+		window.clear(); 
 
-		rs.setPosition(rs.getPosition().x + .01f, rs.getPosition().y); //rs를 현재위치에서 x방향으로 0.01f씩 움직임
+		window.draw(sp); // texture가 아닌 sprite를 그리는것 
 
-		window.draw(rs); // rs를 윈도우에 그려라
-
-		window.display();// 윈도우에 그린 결과물을 출력해라
+		window.display();
 	}
 }
