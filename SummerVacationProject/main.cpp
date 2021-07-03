b2a8793476ff5d632bd045bbd74cc0957f0971ef
phj1027 <qkrgywjd981027@naver.com>
2021-07-03 17:45:26 +0900

    4th SFML
    반복문 이용한 애니메이션 방법

--- SummerVacationProject/main.cpp
+++ SummerVacationProject/main.cpp
@@ -4,61 +4,60 @@
 #include <iostream>
 
 using namespace sf;
+using namespace std;
 
 int main()
 {
-	RenderWindow window(VideoMode(500, 500), "Window"); 
+	RenderWindow window(VideoMode(500, 500), "Window");
 
-	window.setFramerateLimit(60); // 6프레임으로 고정 너무빠르면 이거하기
-
-	IntRect txSq(0, 0, 319 / 3, 424 / 4);  // texture 자를 수 있는 생성자
-		// rectLaft(맨왼쪽) , rectTop(맨위), rectWidth(맨왼쪽에서부터의 길이), rectHeight(맨위부터의 높이)
+	vector<Texture> txVector;
 
 	Texture tx;
-	tx.loadFromFile("Textures/DragonFrames.png"); // 319x424 크기의 사진 
+	char name[150];
 
-	Sprite sp(tx, txSq); // sprite생성자에 tx,txSq를 줌
-	sp.setScale(3.f, 3.f); // 3배
+	for (int i = 1; i <= 12; ++i)
+	{
+		sprintf(name, "Textures/Adventure Time with Finn and Jake/Finn&Jake/Finn&Jake_idle_1~12/Finn & Jake_Idle_00%d.png", i);
+		// cout << name << endl; // 관찰위한 코드 
+		tx.loadFromFile(name);
+		txVector.push_back(tx);
+	}
 
-	Clock clock; // 시간재는 함수 시간관리하는 class
+	Sprite sp;
+	sp.setScale(3.f, 3.f); // 사이즈
+
+	sp.setOrigin(tx.getSize().x / 2.f, tx.getSize().y / 2.f);
+	sp.setPosition(Vector2f(250.f, 250.f)); // 좌상단이 0,0 기준으로 위치설정
+
+	Clock clock;
+
+	size_t keyFrameTime = 0;  
 
 	while (window.isOpen())
 	{
 		window.clear();
 
 		Event e;
-		while (window.pollEvent(e)) // 계속해서 윈도우에 대한 이벤트를 받음
-		// 이벤트가 들어왔을때만 실행 
+		while (window.pollEvent(e)) 
 		{
-			switch (e.type) //이벤트타입뭐냐
+			switch (e.type) 
 			{
-			case Event::Closed: // 윈도우닫는 closed임
+			case Event::Closed:
 			{
-				window.close(); // 이거하게되면 맨 위의isopen이 false되면서 while문빠져나오면서 게임종료
-				//이제 창의 x버튼 눌러서 끌 수 있음 
+				window.close(); 
 				break;
 			}
 			default:
 				break;
 			}
 		}
-		if (clock.getElapsedTime().asSeconds() >= 0.3f) 
-		// clock은 실행될때부터 계속해서 시간이 늘어나 근데 0.3초가되었으면 if문실행
-		{
-			if (txSq.left >= 318 - 106) 
-			// 맨 마지막 텍스쳐 left에 다다랐다면 다시 맨 첫번째 텍스쳐(얘의 left는 0이니까)로 가게 left를 0으로 초기화 
-			{
-				txSq.left = 0; 
-			}
-			else
-			// 아직 맨 마지막 텍스쳐에 도달하지않았다면 다음 텍스쳐로 가게 left 좌표를 늘려줌 
-			{
-				txSq.left += 319 / 3;
-			}
 
-			sp.setTextureRect(txSq); // txSq로 정한 사각형 범위 sp한테 넘겨줌 그만큼 잘라서 tx쓰란것
-			clock.restart(); // 0.3초 되었다면 다시 0초로 초기화 후 재시작 
+		if (clock.getElapsedTime().asSeconds() > 0.2f)
+		{
+			sp.setTexture(txVector.data()[++keyFrameTime % txVector.size()]);
+			clock.restart();
 		}
+
 		window.draw(sp);
 		window.display();
 	}
